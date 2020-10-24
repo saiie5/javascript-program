7aff2bf610b7d264e675b3154dae9539e869c1f3
saiie5 <56484744+saiie5@users.noreply.github.com>
2020-10-24 14:26:48 +0530

    Add files via upload

---
+++ practice2.js
@@ -0,0 +1,39 @@
+
+
+var a =10
+
+ var b =12; var c=13;
+ 
+ if (a<b&&a<c&&b<c){
+    console.log(a)
+    console.log(b)
+    console.log(c)
+    
+}
+else if(b<a&&b<c&&c<a){
+    console.log(b)
+    console.log(c)
+    console.log(a)
+}
+else if (b<a) {
+    console.log(c)
+    console.log(b)
+    console.log(a)
+}
+
+  if (a>b&&a>c&&b>c){
+      console.log(a)
+      console.log(b)
+      console.log(c)
+      
+  }
+  else if(b>a&&b>c&&c>a){
+      console.log(b)
+      console.log(c)
+      console.log(a)
+  }
+  else if (b>a) {
+    console.log(c)
+    console.log(b)
+    console.log(a)
+  }
---
+++ practice3.js
@@ -0,0 +1,164 @@
+var a =18;
+var b = 15;
+(a>b) ? console.log("a is larger") : (a<b) ? console.log("b is larger than a"): console.log("a equal b");
+
+
+
+
+a ="97004676"
+b="970046xZ"
+ 
+if(a===b){
+    console.log("valid passcode")
+}
+else {
+    console.log("wrong passcode")
+}
+
+
+
+
+
+a= 15;
+if(a%2==0){
+    console.log("it os odd")
+}
+else{
+    console.log("it is even")
+}
+
+
+
+
+
+
+
+a=24
+if(a<=13 && a >= 19){
+    console.log("teenage")
+}
+if(a<=20 && a >= 29){
+    console.log("twenties")
+}
+else{
+    console.log("not twenties or teenage")
+}
+
+
+
+
+
+
+
+
+
+var a =10
+
+ var b =12; var c=13;
+ 
+ if (a<b&&a<c&&b<c){
+    console.log(a)
+    console.log(b)
+    console.log(c)
+    
+}
+else if(b<a&&b<c&&c<a){
+    console.log(b)
+    console.log(c)
+    console.log(a)
+}
+else if (b<a) {
+    console.log(c)
+    console.log(b)
+    console.log(a)
+}
+
+  if (a>b&&a>c&&b>c){
+      console.log(a)
+      console.log(b)
+      console.log(c)
+      
+  }
+  else if(b>a&&b>c&&c>a){
+      console.log(b)
+      console.log(c)
+      console.log(a)
+  }
+  else if (b>a) {
+    console.log(c)
+    console.log(b)
+    console.log(a)
+  }
+
+
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+  a=30;
+  b=20;
+   (a==b) ? console.log("a equal b"):("a is not equal to b");
+
+
+
+
+   var day ="monday"
+
+
+   switch (month){
+       case "january" :
+           console.log("jan")
+           break;
+           case "february":
+               console.log("feb")
+               break;
+               case "march":
+                   console.log("mar")
+                   break;
+                   case "april":
+                       console.log("apr")
+                       break;
+                       case "may":
+                           console.log("may")
+                           break;
+                           case("june"):
+                           console.log("jun")
+                           break;
+                           case ("july"):
+                               console.log("jul")
+                               break;
+                               case(augest):
+                               console.log("aug")
+                               break;
+                               case("septeber"):
+                               console.log("sep")
+                               break;
+                               case("october"):
+                               console.log("oct")
+                               break;
+                               case("november"):
+                               console.log("nov")
+                               break;
+                               case("december"):
+                               console.log("dec")
+                               break;
+
+
+
+
+   
+
+   }
+
+
+
